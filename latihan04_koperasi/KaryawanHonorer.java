28d4345a74edfc746bdde74325cf2a11ea71ae37
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2024-10-05 13:47:32 +0700

    ok

---
+++ latihan04_koperasi/KaryawanHonorer.java
@@ -0,0 +1,43 @@
+package latihan04_koperasi;
+
+public class KaryawanHonorer {
+    private String nik, nama, golongan;
+    private int bulan;
+    
+    public KaryawanHonorer(String nk, String nm, String gl, int bl) {
+        this.nik = nk;
+        this.nama = nm;
+        this.golongan = gl;
+        this.bulan = bl;
+    }
+
+    public void infoPinjam(){
+        int pinjaman = 0;
+        
+        if(this.golongan.equalsIgnoreCase("A")){
+            pinjaman = 30000000;
+        }else if(this.golongan.equalsIgnoreCase("B")){
+            pinjaman = 20000000;
+        }else if(this.golongan.equalsIgnoreCase("C")){
+            pinjaman = 10000000;
+        }
+
+        double bunga = (double) pinjaman * 0.025;
+        int jumlah = (pinjaman + (int)bunga);
+        int perbulan = jumlah / bulan;
+        
+        System.out.println("--- INFO PEMINJAM ---");
+        System.out.println("NIK : \t\t"+ this.nik);
+        System.out.println("Nama : \t\t"+ this.nama);
+        System.out.println();
+        System.out.println("--- DETIL PINJAMAN ---");
+        System.out.println(String.format("Pinjaman: \tRp.%,2d", pinjaman));
+        System.out.println(String.format("Bunga: \t\tRp.%,2d", (int)bunga));
+        System.out.println("-----------------------------");
+        System.out.println(String.format("Jumlah: \tRp.%,2d", jumlah));
+        System.out.println("=============================");
+        System.out.println(String.format("Bulanan: \tRp.%,2d ", perbulan));
+        System.out.println(String.format("Selama: \t%d bulan", bulan));   
+    }
+    
+}
---
+++ latihan04_koperasi/Main.java
@@ -0,0 +1,80 @@
+package latihan04_koperasi;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static int pilih = 0;
+    
+    public static void main(String[] args) {
+        do {
+            boolean valid = false;
+            mainMenu();
+
+            // memastikan user harus input integer
+            while (!valid) {
+                System.out.print("[ Ketik 1/2/3 ]: ");
+
+                if (sc.hasNextInt()) {
+                    pilih = sc.nextInt();
+
+                    if(pilih > 4){
+                        System.out.println("[ Menu salah!, Coba lagi. ]");
+                        valid = false;
+                    }else{
+                        valid = true;        
+                    }
+                } else {
+                    System.out.println("[ Coba lagi! ]");
+                    sc.next();
+                    valid = false;
+                }
+            }
+
+            if (pilih == 1) {
+                mainInputKaryawan(1); // karyawan tetap
+            } else if (pilih == 2) {
+                mainInputKaryawan(2); // karyawan honorer
+            }
+
+        } while (pilih != 3);
+    }
+
+    private static void mainInputKaryawan(int tipe) {
+        String nik, nama, golongan;
+        int bulan;
+
+        sc.nextLine();
+        System.out.print("NIK: ");
+        nik = sc.nextLine();
+        System.out.print("Nama: ");
+        nama = sc.nextLine();
+
+        //menu golongan
+        System.out.print("Golongan [ A/B/C ]: ");
+        golongan = sc.nextLine();
+
+        System.out.print("Lama pinjaman (bln): ");
+        bulan = sc.nextInt();
+
+        if(tipe == 1){ // karyawan tetap
+            KaryawanTetap k = new KaryawanTetap(nik, nama, golongan, bulan);
+            k.infoPinjam();
+        }else if(tipe == 2){ // karyawan honorer
+            KaryawanHonorer k = new KaryawanHonorer(nik, nama, golongan, bulan);
+            k.infoPinjam();        
+        }
+        
+        System.out.print("[Tekan apapun untuk lanjut]");
+        sc.nextLine();
+        sc.nextLine();
+    }
+
+    private static void mainMenu() {
+        System.out.println("-----------------------");
+        System.out.println("1. Karyawan Tetap");
+        System.out.println("2. Karyawan Honorer");
+        System.out.println("-----------------------");
+    }
+}
