39e40b9e94fe05eaa430c6c84a94d90ca91d6009
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2024-12-02 21:30:28 +0700

    ok

---
+++ latihan07_overloading_lab/Mahasiswa.java
@@ -0,0 +1,60 @@
+package latihan07_overloading_lab;
+
+import java.util.ArrayList;
+
+public class Mahasiswa {
+    private String nama;
+    private String npm;
+    private double ipk;
+
+    // Konstruktor default
+    public Mahasiswa() {
+        this.nama = "Belum diisi";
+        this.npm = "Belum diisi";
+        this.ipk = 0.0;
+    }
+
+    // Konstruktor dengan parameter nama dan npm
+    public Mahasiswa(String nama, String npm) {
+        this.nama = nama;
+        this.npm = npm;
+        this.ipk = 0.0;
+    }
+
+    // Konstruktor dengan semua parameter
+    public Mahasiswa(String nama, String npm, double ipk) {
+        this.nama = nama;
+        this.npm = npm;
+        this.ipk = ipk;
+    }
+
+    // Fungsi untuk menampilkan data mahasiswa
+    public void tampilData() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NPM: " + npm);
+        System.out.println("IPK: " + ipk);
+    }
+
+    // Fungsi untuk menghitung rata-rata IPK (overloading)
+    public double hitungRataRataIPK(ArrayList<Mahasiswa> listMahasiswa) {
+        double totalIPK = 0;
+        for (Mahasiswa mhs : listMahasiswa) {
+            totalIPK += mhs.ipk;
+        }
+        return totalIPK / listMahasiswa.size();
+    }
+
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+
+}
